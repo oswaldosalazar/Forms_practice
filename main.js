111542cf035c5a32180ac03cddd0e06021e05f19
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-07-21 22:22:46 -0600

    Files modified again

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ $("#name").on("keyup", function (event) {
   console.log(value);
 });
 $("#name").on("click", function (event) {
-  $(this).css("background-color", "lightgreen")
-  $(this).css("font-size", "30px")
+  $(this).css("background-color", "lightyellow")
+  $(this).css("font-size", "40px")
+});
+$("#output").on("mouseover", function (event) {
+  $(this).css("text-transform", "uppercase")
+  $(this).css("color", "brown")
 });
